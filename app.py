e8dbcc3bc662ad365eb352481a5a0e8a1754237f
gosse <gary@localhost>
2022-12-04 09:24:38 -0500

    fixed templates, added some css

--- app.py
+++ app.py
@@ -10,12 +10,25 @@ def get_recipes():
   for item in list(glob.glob('./static/data/*.yml')):
     with open(item, 'r') as stream:
       recipe = yaml.safe_load(stream)
-      recipe['path'] = item.replace('./static/data/','recipes/')
+      path = item.replace('./static/data/','recipes/')
+      recipe['path'] = path.replace('.yml','')  
       recipes.append(recipe)
   print(recipes)
   return recipes
+
+def get_recipe(name):
+  path = './static/data/' +  name + '.yml'
+  with open(path, 'r') as stream:
+    recipe = yaml.safe_load(stream)
+    print(recipe)
+    return recipe
 
 @app.route('/')
 def index():
   recipes = get_recipes()
   return render_template('index.html', recipes=recipes)
+
+@app.route('/recipes/<name>')
+def recipes(name):
+  recipe = get_recipe(name)
+  return render_template('recipe.html', recipe=recipe)
